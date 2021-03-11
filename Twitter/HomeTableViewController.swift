4bf00c46b8696f127dcc32f1dd5ddc7d112be08b
Chantel <ngohchantel@hotmail.com>
2021-03-11 08:02:56 +0800

    Able to retweet, favourite, and post tweets.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -23,9 +23,14 @@ class HomeTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
         
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
     @objc func loadTweets() {
-        
         numberOfTweet = 20;
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numberOfTweet]
@@ -90,6 +95,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
